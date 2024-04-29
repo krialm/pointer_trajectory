b0d966c30252fa050a158bea0bead6e094238a87
Artem <konotopa20005@gmail.com>
2024-04-30 01:52:49 +0300

    //

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ video_pth = 'D:\GitHubRep\pointer_trajectory\croped_test_video.mp4'
 cap = cv2.VideoCapture(video_pth)
 _, frame = cap.read()
 frame = frame[10:-10, 5:-10]
-contours = detection.get_contours(frame, 233, 80)
+contours = detection.get_contours(frame, 233, 90)
 
 
 
